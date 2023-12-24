a1f176f53e7e8ca3699539ffccfe6ed5c33a084f
“Alberto <aafernandez2005@gmail.com>
2023-12-24 08:22:57 -0400

    Reverse [ADD] my_module: add the start_datetime field to the model my_module.my_module

---
+++ addons/my_module/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "my_module",
+
+    'summary': """
+        This is a test module""",
+
+    'description': """
+        This is a test module 
+    """,
+
+    'author': "AAF Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.2',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
