be4be354ddd1398b52560d52b5b9d1254289d697
“Alberto <aafernandez2005@gmail.com>
2023-12-27 19:33:38 -0400

    1er Commit Customization

---
+++ addons/customization/__manifest__.py
@@ -0,0 +1,24 @@
+
+# -*- coding: utf-8 -*-
+{
+    "name": "Customization",
+    'summary': """
+        Retentions """,
+    "category": "Uncategorized",
+    "version": "0.1",
+    "author": "AAF",
+    "website": "https://www.linkedin.com/in/alberto-a-fern%C3%A1ndez-82948b31/",
+#    "depends": ['fleet'],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/res_partner_ext.xml',
+        
+    ],
+    'application': False,
+    # 'assets': {
+    #     'web.report_assets_common': [
+    #         'nacd_core/static/scss/**/*',
+    #     ],
+}
+}
+
---
+++ addons/customization/models/res_partner_ext.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api, _
+
+class CustomizeResPartner(models.Model):
+    _name = 'customize.res_partner_ext'
+
+    # mot_start_date = fields.Date(string='MOT Start Date')
+    # mot_exp_date = fields.Date(string='MOT Expiry Date')
+    dec_legal_iva = fields.Boolean(string="Declaración Legal de IVA")
+    agen_reten_iva = fields.Boolean(string="Es Agente de Retención (IVA)?")
+    tasa_reten_iva = fields.Float(string="Tasa de Retención de IVA")
+
+    agen_reten_ing = fields.Boolean(string="Agente de Retención de Ingresos?")
+    soc_person_fis = fields.Boolean(string="¿Es una Sociedad de Personas Físicas?")
+    exen_reten_ing = fields.Boolean(string="¿Está Exento de Retención de Ingresos?")
+
+#    mot_service_centre = fields.Many2one('res.partner', string='MOT Service Centre')
+#    vehicle_id = fields.Many2one('fleet.vehicle')
+
+
+class ResPartnerInherit(models.Model):
+    _inherit = 'res.partner'
+
+#    vehicle_mot_ids = fields.One2many('customize.fleet.mot', 'vehicle_id')
