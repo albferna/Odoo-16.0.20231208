a0333cee58d77792b157d6d8163ecfc771e5bd0e
“Alberto <aafernandez2005@gmail.com>
2023-12-28 13:39:20 -0400

    4to Commit Customization

--- addons/customization/models/res_partner_ext.py
+++ addons/customization/models/res_partner_ext.py
@@ -5,13 +5,8 @@ class CustomizeResPartner(models.Model):
 
     # mot_start_date = fields.Date(string='MOT Start Date')
     # mot_exp_date = fields.Date(string='MOT Expiry Date')
-    dec_legal_iva = fields.Boolean(string="Declaración Legal de IVA")
-    agen_reten_iva = fields.Boolean(string="Es Agente de Retención (IVA)?")
-    tasa_reten_iva = fields.Float(string="Tasa de Retención de IVA")
+    
 
-    agen_reten_ing = fields.Boolean(string="Agente de Retención de Ingresos?")
-    soc_person_fis = fields.Boolean(string="¿Es una Sociedad de Personas Físicas?")
-    exen_reten_ing = fields.Boolean(string="¿Está Exento de Retención de Ingresos?")
 
 #    mot_service_centre = fields.Many2one('res.partner', string='MOT Service Centre')
 #    vehicle_id = fields.Many2one('fleet.vehicle')
@@ -19,5 +14,16 @@ class CustomizeResPartner(models.Model):
 
 class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
+    # dec_legal_iva = fields.Boolean(string='Declaración Legal de IVA')
+    dec_legal_iva = fields.Boolean('Declaración Legal de IVA')
+    agen_reten_iva = fields.Boolean('Es Agente de Retención (IVA)?')
+    tasa_reten_iva = fields.Float('Tasa de Retención de IVA')
+
+    agen_reten_ing = fields.Boolean('Agente de Retención de Ingresos?')
+    soc_person_fis = fields.Boolean('¿Es una Sociedad de Personas Físicas?')
+    exen_reten_ing = fields.Boolean('¿Está Exento de Retención de Ingresos?')
+
+
+
 
 #    vehicle_mot_ids = fields.One2many('customize.fleet.mot', 'vehicle_id')
