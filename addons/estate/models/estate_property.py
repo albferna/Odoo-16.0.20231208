d30fa0d8039fb9e4d51a5e951c7fc5b6d4312cc9
“Alberto <aafernandez2005@gmail.com>
2024-01-02 22:53:32 -0400

    fields state_property.py

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -7,3 +7,21 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "CRM Estate Property"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East')
+                   , ('west', 'West')],
+        help="Garden Orientation is used to describe cardinal points")
