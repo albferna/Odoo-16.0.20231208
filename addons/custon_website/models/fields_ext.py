e9a450bf464df3e0019ecae6289b24787366eef7
“Alberto <aafernandez2005@gmail.com>
2024-01-18 14:17:17 -0400

    commit 2 custon_web (curso)

--- addons/custon_website/models/fields_ext.py
+++ addons/custon_website/models/fields_ext.py
@@ -7,4 +7,4 @@ class FieLdsModelExt(models.Model):
     many_to_one = fields.Many2one('product.template', 'Campo Many2one')
     one_to_many = fields.One2many('sale.order', 'many_to_one', 'Campo One2many')
     many_to_many = fields.Many2many('sale.order', 'sale_handle', 'order_id', 'order_handle_id', 'Campo Many2many')
-
+    ## many_to_many = fields.Many2many('sale.order', 'sale_handle', 'order_id', 'order_line', 'Campo Many2many')
